3a4cd6b85965fedfcd35accb2c1a3e7f50904a13
MoeOnose <onose.moe.infratop@gmail.com>
2018-02-01 08:23:46 +0000

    wishesコントローラとcompleteのviewを若干更新しました

---
+++ app/controllers/wishes_controller.rb
@@ -0,0 +1,33 @@
+class WishesController < ApplicationController
+
+	def new
+		@wish = Wish.new
+	end
+
+	def create
+		wish = Wish.new(wish_params)
+		if wish.save?
+			render :action => "confirmation"
+		else
+			render :action => "new"
+		end
+	end
+
+	def confirmation
+		@wish = Wish.find(params[:id])
+	end
+
+	def complete
+		@wish = Wish.find(params[:id])
+	end
+
+	def show
+		@wish = Wish.find(params[:id])
+		@messages = @wish.messages
+	end
+
+	private
+	def wish_params
+		params.require(:wish).permit(:discription, :object, :deadline, :title, :image)
+	end
+end
