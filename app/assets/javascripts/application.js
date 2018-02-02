002ddf28359e2668477283612b4eecb69e6a800f
isc1250275 <bfkk1250275@gn.iwasaki.ac.jp>
2018-02-02 18:31:55 +0900

    js改善

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,32 +20,8 @@
 
 // modal
 $(function(){
-	$("#test0").click(function(){
-        $("#0").modal('show');
-	});
-    $("#test1").click(function(){
-        $("#1").modal('show');
-	});
-    $("#test2").click(function(){
-        $("#2").modal('show');
-	});
-    $("#test3").click(function(){
-        $("#3").modal('show');
-	});
-    $("#test4").click(function(){
-        $("#4").modal('show');
-	});
-    $("#test5").click(function(){
-        $("#5").modal('show');
-	});
-    $("#test6").click(function(){
-        $("#6").modal('show');
-	});
-    $("#test7").click(function(){
-        $("#7").modal('show');
-	});
-    $("#test8").click(function(){
-        $("#8").modal('show');
+	$(".test").click(function(){
+        $(this).next().modal('show');
 	});
 });
  // confetti
